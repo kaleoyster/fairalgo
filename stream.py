0119d511ca6562f832e7b59e18efc0d4181dea97
Akshay Kale <akshaykale915@gmail.com>
2020-12-06 15:58:17 -0600

    added doc strings

--- stream.py
+++ stream.py
@@ -5,6 +5,7 @@ description:
     This algorithm is also sensitive to concept drift
     The algorithm simulates streaming datasets, which is equal to stat
 """
+
 __author__='Akshay Kale'
 __copyright__='GPL'
 __email__='akale@unomaha.edu'
@@ -13,14 +14,23 @@ import numpy
 import random
 import csv
 
-
 def read_header(filename):
+    """
+    description:
+    args:
+    returnsG
+    """
     with open(filename, 'r') as headerFile:
         headerReader = csv.reader(headerFile, delimiter=',')
         header = next(headerReader)
     return header
 
 def return_header():
+    """
+    description:
+    args:
+    returns:
+    """
     header = ['age',
               'workclass',
               'fnlwgt',
@@ -40,6 +50,11 @@ def return_header():
     return header
 
 def read_csv(filename):
+    """
+    description:
+    args:
+    returns:
+    """
     data = list()
     with open(filename, 'r') as csvFile:
         csvReader = csv.reader(csvFile, delimiter=',')
@@ -49,15 +64,28 @@ def read_csv(filename):
     return data
 
 def predict_record(record):
+    """
+    description:
+    args:
+    returns:
+    """
     return record[-1]
 
 def fairness_metric(predictions, groundTruth):
-
+    """
+    description:
+    args:
+    returns:
+    """
+    fairnessMetric = None
     return fairnessMetric
+
 def simulation(data):
     """
     description:
         simulate the online prediction of streaming records
+    args:
+    returns:
     """
     groundTruth = list()
     predicted = list()
@@ -67,18 +95,16 @@ def simulation(data):
         # 2. race
         # 3. native country 
         # 4. sex
-    windowOfData = select_window(data, groudTruth)
-    results = predict_record(windowOfData)
-    fairness = compute_fairness(results)
+    #windowOfData = select_window(data, groudTruth)
+    #results = predict_record(windowOfData)
+    #fairness = compute_fairness(results)
+    for record in data:
+        try:
+            predicted.append(predict_record(record))
+        except:
+            print("Invalid record", record)
 
-#    for record in data:
-#        try:
-#            predicted.append(predict_record(record))
-#        except:
-#            # log errors
-#            print("Invalid record", record)
-
-    return fairnessMetric,
+    return predicted
 
 def main():
     """
@@ -87,7 +113,6 @@ def main():
     filename = 'dataset/adult.data'
     headerfile = 'dataset/adult.names'
     data = read_csv(filename)
-    header = return_header()
     print(simulation(data))
 
 if __name__ =='__main__':
